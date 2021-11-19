69986729d3a059b9e457f7e99816f001bece6acc
H4cory <k.kanev13@gmail.com>
2021-11-19 14:42:25 +0200

    19.11

---
+++ Playground/ZadUP/Red.cpp
@@ -0,0 +1,20 @@
+/*
+Ot klaviaturata se vavezhdat posledovatelno nyakolko tseli polozhitelni chisla,
+sled koeto se vavezhda chisloto nula. Sastavete algoritam i napishete programa,
+koyato namira i izvezhda na ekrana broyat na chislata v nay-dalgata poreditsa ot
+posledovatelni ravni chisla. Naprimer, ako vavedenite chisla sa (4, 5, 5, 6, 6, 6, 8, 0),
+rezultatat tryabva da bade 3, zashtoto ima poreditsa ot tri posledovatelni ravni chisla (6, 6, 6) 
+i tova e nay-dalgata poreditsa ot ravni chisla.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int ;
+
+
+
+    //system("pause");
+    return 0;
+}
---
+++ Playground/ZadUP/mas.cpp
@@ -0,0 +1,22 @@
+/*
+Da se napishe programa, koyato zapalva dvumeren masiv s n reda i
+n stalba po pokazaniya nachin.
+
+    1  2  3  4
+    12 13 14 5
+    11 16 15 6
+    10 9  8  7
+
+    
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  
+
+    //system("Pause");
+    return 0;
+}
